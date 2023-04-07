77ae76e20ba4233dd2f665a76eeadf2221c30428
Anique123 <anique_uddin@live.dk>
2023-04-07 16:38:09 +0200

    Method with no ML

--- analyzer.py
+++ analyzer.py
@@ -22,22 +22,24 @@ def extract_features(packet):
     return features
 
 # Normal Capture without ML prep
-def capture_to_df(cap):    
+def packet_filtering(cap):    
     dataset = []
     for packet in cap:
         if packet.haslayer(TCP):
             features = extract_features(packet)
             dataset.append(features)
-
     coloum_names = ["Src. IP", "Src. Port", "Dst. IP", "Dst. Port", "Time"]
     df = pd.DataFrame(dataset, columns=coloum_names)
     #print(df)
     return df
 
 if __name__ == "__main__":
+    #Defining variables for sniff
     capture_time = 10
     iface = "en0"
 
+    # Running methods
     cap = capture_traffic(iface, capture_time)
-    df = capture_to_df(cap)
+    df = packet_filtering(cap)
+    
     print(df)
