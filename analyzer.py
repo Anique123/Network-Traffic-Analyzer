70b599f400050285fdaab7497616a91c95dee699
Anique123 <anique_uddin@live.dk>
2023-04-07 16:33:39 +0200

    Method without ML

--- analyzer.py
+++ analyzer.py
@@ -1,44 +1,43 @@
 from scapy.all import * 
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-def capture_traffic(capture_time, iface):
-    capture_time = 15
-    iface = "en0"
+# Defining interface and time for capturing of traffic
+def capture_traffic(iface, capture_time):
     cap = sniff(iface=iface, timeout=capture_time)
     return cap
 
+# Defining the features that should be analyzed from the capture
 def extract_features(packet):
     features = []
+    # Possible to customize
     features.append(packet[IP].src)
     features.append(packet[TCP].sport)
     features.append(packet[IP].dst)
     features.append(packet[TCP].dport)
-    features.append(packet[TCP].payload.load)
-    features.append(packet.time)
+    #features.append(str(packet[TCP].payload.load))
+    features.append(pd.to_datetime(packet.time, unit= "s"))
     return features
 
-def preparing_training_data(pac):
+# Normal Capture without ML prep
+def capture_to_df(cap):    
     dataset = []
-    class_labels = {
-        'HTTP': 0,
-        'HTTPS': 1,
-        'DNS': 2,
-        'ARP': 3
-    }
-    for packet in pac:
+    for packet in cap:
         if packet.haslayer(TCP):
             features = extract_features(packet)
-            class_label = class_labels.get(packet[TCP].payload.name, -1)
-            if class_label != -1:
-                dataset.append(features + [class_label])
+            dataset.append(features)
 
-    coloum_names = ["Src. IP", "Src. Port", "Dst. IP", "Dst. Port", "Payload", "Time", "Class"]
+    coloum_names = ["Src. IP", "Src. Port", "Dst. IP", "Dst. Port", "Time"]
     df = pd.DataFrame(dataset, columns=coloum_names)
-
-    # Split into Training and Validation Sets
-    X = df.drop('Class Label', axis=1)  # Features
-    y = df['Class Label']  # Class labels
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+    #print(df)
+    return df
 
 if __name__ == "__main__":
-    pass
+    capture_time = 10
+    iface = "en0"
+
+    cap = capture_traffic(iface, capture_time)
+    df = capture_to_df(cap)
+    print(df)
